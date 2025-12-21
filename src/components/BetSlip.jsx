2e07276ccb98de75cb0032ae905833bb98be5705
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 22:00:35 +0300

    betslip

--- src/components/BetSlip.jsx
+++ src/components/BetSlip.jsx
@@ -3,3 +3,7 @@ import { ChevronRight } from 'lucide-react';
 import Card from '../../common/Card';
 import Button from '../../common/Button';
 
+const BetSlip = () => {
+  const [wager, setWager] = useState(500);
+
+  
