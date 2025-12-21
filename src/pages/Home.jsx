74b9b3122be2db432540789e0e9ac4e0844982e4
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 22:14:14 +0300

    home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import StatsGrid from '../../components/features/dashboard/StatsGrid';
 import Card from '../../components/common/Card';
+import Button from '../../components/common/Button';
+import { Wallet } from 'lucide-react';
