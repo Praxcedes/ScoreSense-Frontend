84c0164e94b7be71214f6b834cd425b9c9b7c0ca
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 21:43:01 +0300

    AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,3 +2,7 @@ import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
 
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({ name: 'Juma', role: 'analyst', points: 12500 });
+
+  
