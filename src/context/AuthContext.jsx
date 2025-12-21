239d16e910f3b430083a0b29b41ed6a46390b789
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 21:43:19 +0300

    AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,4 +5,5 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState({ name: 'Juma', role: 'analyst', points: 12500 });
 
-  
+  const login = (userData) => setUser(userData);
+  const logout = () => setUser(null);
