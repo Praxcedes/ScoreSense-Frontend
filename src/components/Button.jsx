6f3233860027e436c029d2fc0d8daad5e6ea3456
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 20:43:14 +0300

    Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,6 @@
 import React from 'react';
+
+const Button = ({ children , variant = 'primary', className = "", ...props }) => {
+  const baseStyle = "px-4 py-2 rounded-xl font-bold transition-all active:scale-95 flex items-center justify-center gap-2";
+  
+}
