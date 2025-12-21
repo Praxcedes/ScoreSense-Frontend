ef715b5e3e7f7fa098db8e06d51ec9f4bd489dc1
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 21:19:16 +0300

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,3 +7,12 @@ const api = axios.create({
   },
 });
 
+// Add a request interceptor to attach the JWT token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
