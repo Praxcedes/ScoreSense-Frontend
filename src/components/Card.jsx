b26796e3dc5cd0df0ad4d4ac154392b8da2ee2cf
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-21 20:34:00 +0300

    card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const Card = ({ children, className = "" }) => {
+  return (
+    <div className={`bg-surface border border-gray-800 rounded-2xl p-6 ${className}`}>
+      {children}
+    </div>
+  );
+};
