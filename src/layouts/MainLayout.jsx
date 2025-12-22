cd8b4ae0002cdb3ceac94f44ddb64fdbb2ccd8d1
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 21:40:22 +0300

    MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,4 +6,7 @@ const MainLayout = () => {
   return (
     <div className="flex bg-background min-h-screen text-white font-sans">
       <Sidebar />
+      <main className="flex-1 md:ml-64 p-4 md:p-8 transition-all duration-300">
+        <Outlet />
+      </main>
       
