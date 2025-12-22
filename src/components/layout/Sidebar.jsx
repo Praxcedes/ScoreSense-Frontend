cbe328052db3ceb790a13c03af238cf01a5ad402
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 22:46:12 +0300

    Layout

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,9 +1,26 @@
+import { Home, BarChart2, Users } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
-    <aside className="w-64 bg-green-900 p-4">
-      <h2 className="text-xl font-bold text-green-400">
+    <aside className="w-64 bg-black border-r border-green-800 p-4">
+      <h1 className="text-2xl font-bold text-green-400 mb-8">
         ScoreSense
-      </h2>
+      </h1>
+
+      <nav className="space-y-4">
+        <Link to="/" className="flex gap-2 hover:text-green-400">
+          <Home size={18} /> Dashboard
+        </Link>
+
+        <Link to="/predict" className="flex gap-2 hover:text-green-400">
+          <BarChart2 size={18} /> Predict
+        </Link>
+
+        <Link to="/admin" className="flex gap-2 hover:text-green-400">
+          <Users size={18} /> Admin
+        </Link>
+      </nav>
     </aside>
   );
 };
