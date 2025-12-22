d3338f607d6d380f2f6c3044b84921ee97649a90
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 21:41:15 +0300

    MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -9,7 +9,8 @@ const MainLayout = () => {
       <main className="flex-1 md:ml-64 p-4 md:p-8 transition-all duration-300">
         <Outlet />
       </main>
-      </div>
+    </div>
   );
 };
+
 export default MainLayout;
