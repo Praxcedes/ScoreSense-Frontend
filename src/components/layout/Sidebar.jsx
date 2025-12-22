705e3eb672c821cde5839c84ac50d659e239f137
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 22:07:07 +0300

    components

---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <aside className="w-64 bg-gray-900 p-4">
+      <h2 className="text-lg font-bold">ScoreSense</h2>
+    </aside>
+  );
+};
+
+export default Sidebar;
