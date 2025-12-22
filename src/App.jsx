f10e37a2a6d07f575ab3601c536113fc625cb2c3
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 21:43:27 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
+// Layouts
+import MainLayout from './layouts/MainLayout';
+
