b1c1abc4cd09d20cab1ec9da5910bf5300bd8973
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-22 21:40:10 +0300

    MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,4 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/layout/Sidebar';
 
 const MainLayout = () => {
-  
+  return (
+    <div className="flex bg-background min-h-screen text-white font-sans">
+      <Sidebar />
+      
