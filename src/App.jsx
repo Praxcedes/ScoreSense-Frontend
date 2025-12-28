4967953c08e25064d6dc84b9cd4c426bed7448dc
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-28 20:00:16 +0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminLayout from "./layouts/AdminLayout";
+  import { BrowserRouter, Routes, Route } from "react-router-dom";
+  import AdminLayout from "./layouts/AdminLayout";
 
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import Users from "./pages/admin/Users";
-import Tournaments from "./pages/admin/Tournaments";
-import Events from "./pages/admin/Events";
-import Points from "./pages/admin/Points";
-import Config from "./pages/admin/Config";
+  import AdminDashboard from "./pages/admin/AdminDashboard";
+  import Users from "./pages/admin/Users";
+  import Tournaments from "./pages/admin/Tournaments";
+  import Events from "./pages/admin/Events";
+  import Points from "./pages/admin/Points";
+  import Config from "./pages/admin/Config";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="users" element={<Users />} />
-          <Route path="tournaments" element={<Tournaments />} />
-          <Route path="events" element={<Events />} />
-          <Route path="points" element={<Points />} />
-          <Route path="config" element={<Config />} />
+  export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="users" element={<Users />} />
+            <Route path="tournaments" element={<Tournaments />} />
+            <Route path="events" element={<Events />} />
+            <Route path="points" element={<Points />} />
+            <Route path="config" element={<Config />} />
+          </Route>
+          <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Overview />} />
+          <Route path="users" element={<ManageUsers />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+                      
+        </Routes>
+      </BrowserRouter>
+    );
+  }
