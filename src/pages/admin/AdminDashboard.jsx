4aaa22b7da95c87a90657511a8e9430d8134d504
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-28 20:08:28 +0300

    admin

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,3 +1,24 @@
+import AdminStats from "../../components/features/admin/dashboard/AdminStats";
+import RecentPredictions from "../../components/features/admin/dashboard/RecentPredictions";
+
 export default function AdminDashboard() {
-  return <h2 className="text-xl font-semibold">Admin Overview</h2>;
+  return (
+    <div>
+      {/* Header */}
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold text-white">
+          Admin Dashboard
+        </h1>
+        <p className="text-sm text-gray-400">
+          Platform performance overview & analytics
+        </p>
+      </div>
+
+      {/* Stats */}
+      <AdminStats />
+
+      {/* Recent Activity */}
+      <RecentPredictions />
+    </div>
+  );
 }
