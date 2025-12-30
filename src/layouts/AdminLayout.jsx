e332214826fd9b71ef7ceef6d3b5ad58880f9c91
Praxcedes <praxcedeskabeya@gmail.com>
2025-12-30 18:27:19 +0300

    admin

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
-import AdminHeader from "../components/features/admin/common/AdminHeader";
-import AdminTabs from "../components/features/admin/common/AdminTabs";
+import Sidebar from "../components/layout/Sidebar";
 
 export default function AdminLayout() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#08160f] to-black text-white">
-      <AdminHeader />
-      <AdminTabs />
+    <div className="flex min-h-screen bg-black text-white">
+      <Sidebar />
 
-      <main className="px-6 py-6">
+      <main className="flex-1 p-6">
         <Outlet />
       </main>
     </div>
